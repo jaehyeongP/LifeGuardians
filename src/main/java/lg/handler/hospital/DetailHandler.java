23e462ad91cc4a45a17e79ff6def6489a4e03dfb
9min9 <gming0226@gmail.com>
2023-10-10 16:42:45 +0900

    update: Divide hospital detail page

---
+++ src/main/java/lg/handler/hospital/DetailHandler.java
@@ -0,0 +1,20 @@
+package lg.handler.hospital;
+
+import lg.handler.Handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DetailHandler implements Handler {
+
+    @Override
+    public String process(HttpServletRequest request, HttpServletResponse response) {
+        String page = "";
+        if(request.getMethod().equals("GET")) {
+            page = "/page/hospital/detail.jsp";
+        } else {
+
+        }
+        return page;
+    }
+}
