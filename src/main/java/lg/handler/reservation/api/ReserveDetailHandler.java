92b7dccae1daedbb1f87b57cf4f7d3a8816115e8
SMayor <abdortus159@naver.com>
2023-10-19 02:51:32 +0900

    add : ReserveDetailHandler.java

---
+++ src/main/java/lg/handler/reservation/api/ReserveDetailHandler.java
@@ -0,0 +1,56 @@
+package lg.handler.reservation.api;
+
+import lg.handler.Handler;
+import lg.handler.hospital.dto.HospitalListResponse;
+import lg.handler.reservation.dto.ReservationListResponse;
+import lg.hospital.Hospital;
+import lg.hospital.HospitalService;
+import lg.member.Member;
+import lg.member.MemberService;
+import lg.reservation.Reservation;
+import lg.reservation.ReservationService;
+import lg.util.JsonMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReserveDetailHandler implements Handler {
+    @Override
+    public String process(HttpServletRequest request, HttpServletResponse response) {
+        if(request.getMethod().equals("GET")) {
+            ReservationService reservationService = new ReservationService();
+            HospitalService hospitalService = new HospitalService();
+
+            int reserve_id = (int) request.getSession().getAttribute("reserve_id");
+            Reservation reservation = reservationService.getReservationByReserveId(reserve_id);
+            Hospital findHospital = hospitalService.getHospitalByHpid(reservation.getHpid());
+
+
+            ReservationListResponse resultHp =ReservationListResponse.builder()
+                    .hpid(reservation.getHpid())
+                    .dutyName(findHospital.getDutyName())
+                    .dutyDivName(findHospital.getDutyDivName())
+                    .dutyTel1(findHospital.getDutyTel1())
+                    .address(findHospital.getAddress())
+                    .extraAddress(findHospital.getExtraAddress())
+                    .reserveTime(reservation.getReserveTime())
+                    .symptoms(reservation.getSymptoms())
+                    .status(reservation.getStatus())
+                    .reserveId(String.valueOf(reservation.getReserve_id())).build();
+
+            String jsonStr = JsonMapper.objectToJson(resultHp);
+
+            try {
+                response.getWriter().write(jsonStr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+}
